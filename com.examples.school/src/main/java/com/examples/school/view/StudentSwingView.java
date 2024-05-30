6d9a23de39e036e0d136032aadb373e614e6acb3
Lorenzo Maffucci <lorenzo.maffucci@stud.unifi.it>
2024-05-30 15:49:45 +0200

    First tests and implementations

--- com.examples.school/src/main/java/com/examples/school/view/StudentSwingView.java
+++ com.examples.school/src/main/java/com/examples/school/view/StudentSwingView.java
@@ -5,20 +5,44 @@ import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.DefaultListModel;
 import javax.swing.border.EmptyBorder;
 
 import com.examples.school.model.Student;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class StudentSwingView extends JFrame implements StudentView {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtId;
+	private JLabel lblName;
+	private JTextField txtName;
+	private JButton btnAdd;
+	private JScrollPane scrollPane;
+	private JButton btnDeleteSelected;
+	private JLabel lblErrorMessage;
+	
+	private JList<Student> listStudents;
+	private DefaultListModel<Student> listStudentsModel;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					StudentSwingView frame = new StudentSwingView();
@@ -29,28 +53,129 @@ public class StudentSwingView extends JFrame implements StudentView {
 			}
 		});
 	}
+	
+	DefaultListModel<Student> getListStudentsModel() {
+		return listStudentsModel;		
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public StudentSwingView() {
+				
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblId = new JLabel("id");
+		GridBagConstraints gbc_lblId = new GridBagConstraints();
+		gbc_lblId.insets = new Insets(0, 0, 5, 5);
+		gbc_lblId.anchor = GridBagConstraints.EAST;
+		gbc_lblId.gridx = 0;
+		gbc_lblId.gridy = 0;
+		contentPane.add(lblId, gbc_lblId);
+				
+		txtId = new JTextField();
+		KeyAdapter btnAddEnabler = new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				btnAdd.setEnabled(
+					!txtId.getText().trim().isEmpty() && 
+					!txtName.getText().trim().isEmpty()
+				);
+			}
+		};
+		txtId.addKeyListener(btnAddEnabler);
+		txtId.setName("idTextBox");
+		GridBagConstraints gbc_txtId = new GridBagConstraints();
+		gbc_txtId.insets = new Insets(0, 0, 5, 0);
+		gbc_txtId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtId.gridx = 1;
+		gbc_txtId.gridy = 0;
+		contentPane.add(txtId, gbc_txtId);
+		txtId.setColumns(10);
+		
+		lblName = new JLabel("name");
+		GridBagConstraints gbc_lblName = new GridBagConstraints();
+		gbc_lblName.anchor = GridBagConstraints.EAST;
+		gbc_lblName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblName.gridx = 0;
+		gbc_lblName.gridy = 1;
+		contentPane.add(lblName, gbc_lblName);
+		
+		txtName = new JTextField();
+		txtName.addKeyListener(btnAddEnabler);
+		txtName.setName("nameTextBox");
+		GridBagConstraints gbc_txtName = new GridBagConstraints();
+		gbc_txtName.insets = new Insets(0, 0, 5, 0);
+		gbc_txtName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtName.gridx = 1;
+		gbc_txtName.gridy = 1;
+		contentPane.add(txtName, gbc_txtName);
+		txtName.setColumns(10);
+		
+		btnAdd = new JButton("Add");
+		btnAdd.setEnabled(false);
+		GridBagConstraints gbc_btnAdd = new GridBagConstraints();
+		gbc_btnAdd.insets = new Insets(0, 0, 5, 0);
+		gbc_btnAdd.gridwidth = 2;
+		gbc_btnAdd.gridx = 0;
+		gbc_btnAdd.gridy = 2;
+		contentPane.add(btnAdd, gbc_btnAdd);
+		
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridwidth = 2;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 3;
+		contentPane.add(scrollPane, gbc_scrollPane);
+		
+		listStudentsModel = new DefaultListModel<>();
+		listStudents = new JList<>(listStudentsModel);		
+		listStudents.addListSelectionListener(e -> btnDeleteSelected.setEnabled(listStudents.getSelectedIndex() != -1));
+		listStudents.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listStudents.setName("studentList");
+		scrollPane.setViewportView(listStudents);
+		
+		btnDeleteSelected = new JButton("Delete Selected");
+		btnDeleteSelected.setEnabled(false);
+		GridBagConstraints gbc_btnDeleteSelected = new GridBagConstraints();
+		gbc_btnDeleteSelected.insets = new Insets(0, 0, 5, 0);
+		gbc_btnDeleteSelected.gridwidth = 2;
+		gbc_btnDeleteSelected.gridx = 0;
+		gbc_btnDeleteSelected.gridy = 4;
+		contentPane.add(btnDeleteSelected, gbc_btnDeleteSelected);
+		
+		lblErrorMessage = new JLabel(" ");
+		lblErrorMessage.setName("errorMessageLabel");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 0, 5);
+		gbc_label.gridx = 0;
+		gbc_label.gridy = 5;
+		contentPane.add(lblErrorMessage, gbc_label);
+		
 	}
 
 	@Override
 	public void showAllStudents(List<Student> students) {
-		// TODO Auto-generated method stub
+		students.stream().forEach(listStudentsModel::addElement);
 		
 	}
 
 	@Override
 	public void showError(String message, Student student) {
-		// TODO Auto-generated method stub
+		lblErrorMessage.setText(message + ": " + student);
 		
 	}
 
--- com.examples.school/src/test/java/com/examples/school/view/StudentSwingViewTest.java
+++ com.examples.school/src/test/java/com/examples/school/view/StudentSwingViewTest.java
@@ -1,17 +1,22 @@
 package com.examples.school.view;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JButtonFixture;
+import org.assertj.swing.fixture.JTextComponentFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.runner.RunWith;
+
+import com.examples.school.model.Student;
 
 @RunWith(GUITestRunner.class)
 public class StudentSwingViewTest extends AssertJSwingJUnitTestCase {
@@ -33,6 +38,66 @@ public class StudentSwingViewTest extends AssertJSwingJUnitTestCase {
 	@Test @GUITest
 	public void testControlsInitialStates() {
 		window.label(JLabelMatcher.withText("id"));
+		window.textBox("idTextBox").requireEnabled();
+		window.label(JLabelMatcher.withText("name"));
+		window.textBox("nameTextBox").requireEnabled();
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
+		window.list("studentList");
+		window.button(JButtonMatcher.withText("Delete Selected")).requireDisabled();
+		window.label("errorMessageLabel").requireText(" ");
 	}
-
+	
+	@Test
+	public void testWhenIdAndNameAreNotEmptyThenAddButtonShouldBeEnabled() {
+		window.textBox("idTextBox").enterText("1");
+		window.textBox("nameTextBox").enterText("test");
+		window.button(JButtonMatcher.withText("Add")).requireEnabled();
+	}
+	
+	@Test
+	public void testWhenEitherIdOrNameAreBlankThenAddButtonShouldBeDisabled() {
+		
+		JTextComponentFixture idTextBox = window.textBox("idTextBox");
+		JTextComponentFixture nameTextBox = window.textBox("nameTextBox");
+		
+		idTextBox.enterText("1");
+		nameTextBox.enterText(" ");
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
+		
+		idTextBox.setText("");
+		nameTextBox.setText("");
+		
+		idTextBox.enterText(" ");
+		nameTextBox.enterText("test");
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
+		
+	}
+	
+	@Test
+	public void testDeleteButtonShouldBeEnabledOnlyWhenAStudentIsSelected() {
+		GuiActionRunner.execute(() -> 
+			studentSwingView.getListStudentsModel().addElement(new Student("1", "test")));
+		window.list("studentList").selectItem(0);
+		JButtonFixture deleteButton = window.button(JButtonMatcher.withText("Delete Selected"));
+		deleteButton.requireEnabled();
+		window.list("studentList").clearSelection();
+		deleteButton.requireDisabled();
+	}
+	
+	@Test
+	public void testShowAllStudentsShouldAddStudentDescriptionsToTheList() {
+		Student student1 = new Student("1", "test1");
+		Student student2 = new Student("2", "test2");
+		GuiActionRunner.execute(() -> studentSwingView.showAllStudents(Arrays.asList(student1, student2)));
+		String[] listContents = window.list().contents();
+		assertThat(listContents).containsExactly(student1.toString(), student2.toString());
+	}
+	
+	@Test
+	public void testShowErrorShouldShowTheMessageInTheErrorLabel() {
+		Student student = new Student("1", "test1");
+		GuiActionRunner.execute(() -> studentSwingView.showError("error message", student));
+		window.label("errorMessageLabel").requireText("error message: " + student);
+	}
+	
 }
